3db882818ef0d328967b9883bde0b3bee6f09a5c
cdanmontoya <cdanmontoya@experimentality.co>
2023-03-11 01:13:30 -0500

    Refactor

--- pregunta.py
+++ pregunta.py
@@ -18,16 +18,16 @@ def ingest_data():
     column_limits = [(0, 9), (9, 25), (25, 41), (41, 118)]
     column_names = get_column_names(column_limits)
 
-    df = (pd.read_fwf('clusters_report.txt', skiprows=4, colspecs=column_limits, header=None, names=column_names)
-          .fillna(method='ffill')
-          .astype({"cluster": int, "cantidad_de_palabras_clave": int})
-          .groupby([c for c in column_names if c != "principales_palabras_clave"], as_index=False)[column_names[3]]
-          .apply(clean_key_words())
-          )
-
-    df[column_names[2]] = df[column_names[2]].str.replace('%', '').str.replace(',', '.').str.strip().astype(float)
-
-    return df
+    return (pd.read_fwf('clusters_report.txt',
+                        skiprows=4,
+                        colspecs=column_limits,
+                        header=None,
+                        names=column_names,
+                        converters={'porcentaje_de_palabras_clave': clean_percentage()})
+            .fillna(method='ffill')
+            .astype({"cluster": int, "cantidad_de_palabras_clave": int, 'porcentaje_de_palabras_clave': float})
+            .groupby([c for c in column_names if c != "principales_palabras_clave"], as_index=False)[column_names[3]]
+            .apply(clean_keywords()))
 
 
 def get_column_names(limits: List[Tuple[int, int]]) -> List[str]:
@@ -37,7 +37,11 @@ def get_column_names(limits: List[Tuple[int, int]]) -> List[str]:
             .values)
 
 
-def clean_key_words():
+def clean_percentage():
+    return lambda x: x.replace('%', '').replace(',', '.').strip()
+
+
+def clean_keywords():
     return lambda x: ' '.join(x.apply(
         lambda y: ' '.join(y.replace('.', '').split())
     ))
